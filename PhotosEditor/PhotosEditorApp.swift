c5f209dd4210f8e93cf5473be1f012d34aa9dd85
Лидия Пятаева <z_liya@inbox.ru>
2024-09-11 16:57:39 +0500

    Initial Commit

---
+++ PhotosEditor/PhotosEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosEditorApp.swift
+//  PhotosEditor
+//
+//  Created by Лидия Пятаева on 11.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct PhotosEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
